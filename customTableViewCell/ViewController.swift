f42f549feb8853d3efa734e518f99a9f7e531d70
shashwat-code <71892990+shashwat-code@users.noreply.github.com>
2021-09-27 19:48:09 +0530

    Initial Commit

---
+++ customTableViewCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  customTableViewCell
+//
+//  Created by Shashwat on 27/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
