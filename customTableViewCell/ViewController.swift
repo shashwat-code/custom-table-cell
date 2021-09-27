d5948b60fb80570fe6af2c100e821c7f718b1bf8
shashwat-code <71892990+shashwat-code@users.noreply.github.com>
2021-09-28 00:08:27 +0530

    no big changes done

--- customTableViewCell/ViewController.swift
+++ customTableViewCell/ViewController.swift
@@ -7,6 +7,8 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     var table = TableView(frame: .zero, style: .insetGrouped)
     //print("oh")
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(table)
